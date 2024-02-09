d3a94211897131a760da9ad0180a71ba0b9e2031
Victor Redler <victor.redler@gmail.com>
2024-02-09 17:38:25 +0100

    Add home screen concept

---
+++ src/components/LoginIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Components.css';
+
+export default function LoginIcon() {
+    return(
+        <div className='loginIcon'>
+            <i class="fa-solid fa-arrow-right-to-bracket fa-xl"></i>
+            <div className='padding'></div>
+            LOGIN
+        </div>
+    );
+}
